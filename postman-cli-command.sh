e755b60b71b8fc995d08e12e544074fa72c1a8cc
koushindra kumar <koushindrakumarji@gmail.com>
2023-03-21 04:52:48 -0400

    postman-users api added

---
+++ postman-cli-command.sh
@@ -0,0 +1,3 @@
+postman login --with-api-key PMAK-641970063a51d65e591247b5-b74c8136b9d517d12d71d6c4781bfd886f
+postman collection run "postman/collections/AUTH-COLLECTION.json"
+postman collection run "postman/collections/USERS.json"
