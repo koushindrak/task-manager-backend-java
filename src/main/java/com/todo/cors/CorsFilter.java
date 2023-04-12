f6ec05d80a40022bb56459761c865e42c4d01ebc
koushindrak <127419668+koushindrak@users.noreply.github.com>
2023-04-11 23:06:55 -0400

    Cors Enabled

--- src/main/java/com/todo/cors/CorsFilter.java
+++ src/main/java/com/todo/cors/CorsFilter.java
@@ -1,16 +1,19 @@
-//package com.todo.cors;
-//
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//@EnableWebMvc
-//public class CorsFilter implements WebMvcConfigurer {
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**")
-//                .allowedOrigins("*")
-//                .allowedMethods("GET", "OPTIONS", "POST", "PUT", "DELETE", "HEAD");
-//    }
-//
-//}
+package com.todo.cors;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebMvc
+@Slf4j
+public class CorsFilter implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        log.info("\n\n\n=================== ENABLELING CORS 1=============================");
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "OPTIONS", "POST", "PUT", "DELETE", "HEAD");
+    }
+
+}
--- src/main/java/com/todo/cors/MyCORSFilter.java
+++ src/main/java/com/todo/cors/MyCORSFilter.java
@@ -3,20 +3,24 @@ package com.todo.cors;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import java.io.IOException;
 
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE)
-//@EnableWebMvc
+@EnableWebMvc
+@Slf4j
 public class MyCORSFilter implements Filter {
 
 
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        log.info("\n\n\n=================== ENABLELING CORS 1=============================");
 
         HttpServletRequest request = (HttpServletRequest) req;
         HttpServletResponse response = (HttpServletResponse) res;
